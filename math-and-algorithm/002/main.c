6d99a17265e8906aa7b5d6ae1688d780f8d8c44e
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-11 18:12:42 +0900

    002 - Sum of 3 Integers

---
+++ math-and-algorithm/002/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int a1;
+	int a2;
+	int a3;
+
+	scanf("%d%d%d",&a1,&a2,&a3);
+	// printf("%d %d %d\n",a1,a2,a3);
+
+	int total = a1 + a2 + a3;
+	printf("%d\n", total);
+
+	return (0);
+}
